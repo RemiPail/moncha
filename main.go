f4d137a2ee1d2594eb9c4a9a5a5a5ad174ec08ad
Rémi Pailharey <remipailharey@gmail.com>
2022-08-08 19:22:50 +0200

    feat: random endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 )
@@ -12,7 +13,16 @@ func main() {
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, world!\n")
 	}
+
+	randomHandler := func(w http.ResponseWriter, req *http.Request) {
+		choices := [2]string{"Pile", "Face"}
+		pick := choices[rand.Intn(len(choices))]
+
+		io.WriteString(w, pick)
+	}
+
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/random", randomHandler)
 	log.Println("Listening for" + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
