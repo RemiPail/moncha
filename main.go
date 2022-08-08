a2ab1e25a9cbc6209858a24a6b0b838f5f0c1617
Rémi Pailharey <remipailharey@gmail.com>
2022-08-08 23:53:15 +0200

    feat: random among given elements

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"math/rand"
@@ -10,14 +11,18 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	helloHandler := func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello, world!\n")
 	}
 
-	randomHandler := func(w http.ResponseWriter, req *http.Request) {
-		choices := [3]string{"Pile", "Face", "Dans ton cul !!!"}
+	randomHandler := func(w http.ResponseWriter, r *http.Request) {
+		var choices []string
+		err := json.NewDecoder(r.Body).Decode(&choices)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 		pick := choices[rand.Intn(len(choices))]
-
 		io.WriteString(w, pick)
 	}
 
